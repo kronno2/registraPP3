4ceb4bc0fa835d6eeea61a21495f55a7652b17e6
kronno2 <nic.acuna@duocuc.cl>
2023-11-23 18:04:53 -0300

    experiencia 3 app moviles regisapp

---
+++ app/apicrud.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IUsuariop, Users } from './pages/interfaces/interface';
+import { environment } from 'src/environments/environment';
+import { IUsuario } from './pages/interfaces/interface';  
+@Injectable({
+  providedIn: 'root'
+})
+export class ApicrudService {
+
+  constructor(private httpclient:HttpClient) { }
+  listarUsuario():Observable<IUsuariop>{
+    return this.httpclient.get<IUsuariop>(`${environment.apiUrl}/usuario`);
+  }
+  crearUser(newAnimalito: Users): Observable<Users>{
+    return this.httpclient.post<Users>(`${environment.apiUrl}/usuarios`, newAnimalito);
+  }
+
+    checkUsernameExists(username: string): Observable<boolean> {
+    return this.httpclient.get<boolean>(`${environment.apiUrl}/usuarios/exists/${username}`);
+  }
+}
---
+++ app/pages/interfaces/interface.ts
@@ -0,0 +1,18 @@
+export interface IUsuariop {
+    id: number;
+    nombre: string;
+    edad: string;
+    carrera: string;
+  }
+export interface IUsuario {
+  nombre:string;
+  edad:string;
+  carrera: string;
+}
+
+export interface Users {
+  name: string;
+  username: String;
+  password: string;
+  role: string;
+}
